3ddc5cc1383ecc1f8751ae6cd9ae02988673483c
Chloe Fisher <dev@chlofisher.com>
2025-08-16 12:14:29 +0100

    Merge docs into new folder
    Re-add astro dependency to docs
    
    .

---
+++ docs/astro.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'BioShift',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/chlofisher/bioshift' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Getting Started', slug: 'guides/getting_started' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+            editLink: {
+                baseUrl: 'https://github.com/chlofisher/bioshift/edit/docs'
+            },
+            customCss: [
+                './src/styles/style.css'
+            ]
+		}),
+	],
+});
