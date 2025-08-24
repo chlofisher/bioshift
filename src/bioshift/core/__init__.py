d16a33e54bba2884862e5730b25521afe8fb77ea
Chloe Fisher <dev@chlofisher.com>
2025-08-24 13:19:22 +0100

    Create assignment model

--- src/bioshift/core/__init__.py
+++ src/bioshift/core/__init__.py
@@ -1,6 +1,6 @@
 from .spectrum import Spectrum
 from .spectrumdatasource import SpectrumDataSource, TransformedDataSource, SumDataSource
-from .assignment import Assignment
+from .assignment import Assignment, init_protein_structure, import_protein_structure
 from .nucleus import NMRNucleus
 from .peak import Peak, PeakList
 from .spectrumtransform import SpectrumTransform
